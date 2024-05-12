ebe76cf3de5abe4f9187ddf4bdf924ba5f6ece01
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-12 23:35:37 +0530

    Slider working fine! After every 3 seconds slide runs!

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-let currentSlide = 1
+let currentSlide = 1,
+  isSliding = true,
+  interval
 
+// Change Slide Logic
 const changeSlides = () => {
   const slides = document.querySelectorAll('.slide')
+
+  if (currentSlide > slides.length) {
+    currentSlide = 1
+  }
+
   slides.forEach(slide => {
-    const slideNumber = slide.dataset.slide
-    console.log(slideNumber)
+    const slideNumber = slide.dataset.slide * 1
+    if (slideNumber === currentSlide) {
+      slide.classList.add('show-slide')
+      slide.classList.remove('hide-slide')
+    } else {
+      slide.classList.add('hide-slide')
+      slide.classList.remove('show-slide')
+    }
   })
 }
 
-changeSlides()
+// Play Slide show after 3 seconds
+const playPause = () => {
+  if (isSliding) {
+    interval = setInterval(() => {
+      current++
+      changeSlides(currentSlide)
+    }, 3000)
+  } else {
+    clearInterval(interval)
+  }
+}
+
+playPause()
