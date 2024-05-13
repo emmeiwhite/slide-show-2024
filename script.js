4eb37b354d9cc49ed323cba86c63c75641776f6d
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-13 14:16:17 +0530

    Step-2: Done, PlayPause functionality logic also works fine

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let currentSlide = 1
+let currentSlide = 1,
+  interval,
+  isSliderMoving = true
+
+const slides = [...document.querySelectorAll('.slide')]
 
 const changeSlide = () => {
-  const slides = [...document.querySelectorAll('.slide')]
-
   slides.forEach(slide => {
     const slideNumber = slide.dataset.slide * 1
     if (slideNumber === currentSlide) {
@@ -15,4 +17,41 @@ const changeSlide = () => {
   })
 }
 
-changeSlide()
+/** --- Step-2: To autoplay the Carousal --- */
+const playCarousalLoop = () => {
+  interval = setInterval(() => {
+    currentSlide++
+    if (currentSlide > slides.length) {
+      currentSlide = 1
+    } else if (currentSlide < 0) {
+      currentSlide = slides.length
+    }
+
+    changeSlide()
+    console.log(currentSlide)
+  }, 2000)
+}
+
+/** --- 2. PLAY/PAUSE FUNCTIONALITY --- */
+const playPauseContainer = document.querySelector('.play-pause')
+
+playPauseContainer.addEventListener('click', () => {
+  const icon = playPauseContainer.querySelector('.fas')
+
+  if (isSliderMoving) {
+    icon.classList.remove('fa-pause')
+    icon.classList.add('fa-play')
+    isSliderMoving = false
+
+    // To Stop the carousal from moving, we need to use clearInterval
+    clearInterval(interval)
+  } else {
+    icon.classList.add('fa-pause')
+    icon.classList.remove('fa-play')
+    isSliderMoving = true
+
+    playCarousalLoop()
+  }
+})
+
+playCarousalLoop() // This starts the automatic loop
